8a185a53d28db7ac2fe9b7eea6058b316ce916d8
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2026-02-22 11:41:04 +0100

    Update resources

--- src/lib/philips-hue.ts
+++ src/lib/philips-hue.ts
@@ -277,7 +277,7 @@ class PhilipsHue {
     this.updateLights().catch((error) => log.error("Updating lights failed:", error));
   }
 
-  private handleEventStreamUpdate(event: HueEvent) {
+  private async handleEventStreamUpdate(event: HueEvent) {
     for (const data of event.data) {
       if (["light", "grouped_light"].includes(data.type)) {
         let entityId: string;
@@ -297,6 +297,7 @@ class PhilipsHue {
           log.error("Syncing lights failed for event stream update:", error)
         );
 
+        // grouped_light can't be updated, they are a compound of multiple devices and belong to a room/zone
         if (data.type === "light") {
           const groupIds = this.lightIdToGroupIds.get(data.id);
           if (groupIds) {
@@ -307,6 +308,39 @@ class PhilipsHue {
               );
             }
           }
+
+          // a light can only be updated with its name
+          if (data.metadata && typeof data.metadata === "object" && "name" in data.metadata) {
+            const lightConfig = this.config.getLight(data.id);
+            if (!lightConfig) {
+              log.debug("No config found for light %s, skipping config update", data.id);
+              continue;
+            }
+            this.config.updateLight(data.id, {
+              name: data.metadata.name as string,
+              features: lightConfig.features,
+              gamut_type: lightConfig.gamut_type,
+              mirek_schema: lightConfig.mirek_schema
+            });
+          }
+        }
+      } else if (["room", "zone"].includes(data.type)) {
+        const group = this.config.getLight(data.id) as GroupConfig;
+        if (group) {
+          // update the whole group resource if something has changed in it since it is made up of multiple resources
+          const updateGroupData = await this.hueApi.groupResource.getGroupResource(data.id, group.groupType);
+          this.config.updateLight(data.id, {
+            name: updateGroupData.metadata.name,
+            features: getGroupFeatures(updateGroupData),
+            groupType: updateGroupData.type === "zone" ? "zone" : "room",
+            groupedLightIds: updateGroupData.grouped_lights.map((gl) => gl.id),
+            childLightIds: updateGroupData.lights.map((light) => light.id),
+            gamut_type: getMostCommonGamut(updateGroupData),
+            mirek_schema: getMinMaxMirek(updateGroupData)
+          });
+          this.syncGroupState(data.id, updateGroupData).catch((error) =>
+            log.error("Syncing group state failed for event stream update:", error)
+          );
         }
       }
     }
@@ -323,10 +357,8 @@ class PhilipsHue {
         case "room":
         case "zone":
           {
-            const groupData = await this.hueApi.groupResource.getGroupResources(data.type);
-            if (groupData.length > 0) {
-              addAvailableGroups(groupData, data.type, this.config);
-            }
+            const group = await this.hueApi.groupResource.getGroupResource(data.id, data.type);
+            addAvailableGroups([group], data.type, this.config);
           }
           break;
       }
